8882276ee1cacd39aa194c5d0c361313081536d7
BlokOne <90694470+BlokOne@users.noreply.github.com>
2021-10-05 14:06:40 +0300

    added newspage

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const   slides = document.querySelectorAll('.slide');
+        dots = document.querySelectorAll('.dot');
+       
+let index = 0;
+
+
+
+const nextSlide = () => {
+    if(index == slides.length -1) {
+        index = 0; 
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+  
+}
+
+const activeDot = n => {
+    for (dot of dots) {
+        dot.classList.remove("active");
+    }
+    dots[n].classList.add("active");
+}
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove("active");
+    }
+    slides[n].classList.add("active")
+}
+
+dots.forEach((item, indexDot)=> {
+    item.addEventListener("click", () => {
+        index = indexDot;
+        prepareCurrentSlide();
+    })
+})
+
+
+const  timerId = setInterval(nextSlide, 2000);
+
+const prepareCurrentSlide = ind => {
+    activeSlide(index);
+    activeDot(index);
+}
